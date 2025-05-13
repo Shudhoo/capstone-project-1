4ee9b698c925fae5cf288030fcdace01ce032fda
Shudhoo <140329641+Shudhoo@users.noreply.github.com>
2025-05-13 17:45:14 +0530

    Add files via upload

---
+++ Ansible/install_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install default-jre -y
+
+sleep 20
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
---
+++ Ansible/install_slaves.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install default-jre -y
+
+sleep 20
+
+sudo apt install docker.io -y
+
+
